5f1c01d5f0a152f74345e8b6dcc5a6960bfce6b9
Brandon Nuñez <167320871+BrandonDeveloper08@users.noreply.github.com>
2026-01-23 17:56:34 -0500

    Update CV link to point to CVV-WIN.pdf

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -36,7 +36,7 @@ const Introduction = () => {
                         <Link href="/portfolio" className="px-3 py-2 my-2 transition-all border-2 cursor-pointer text-md w-fit rounded-xl hover:shadow-xl hover:shadow-white/50">
                             Ver proyectos
                         </Link>
-                        <a href="./CVV.pdf"
+                        <a href="./CVV-WIN.pdf"
                             className="px-3 py-2 my-5 transition-all border-2 cursor-pointer text-md w-fit text-secondary border-secondary rounded-xl hover:shadow-xl hover:shadow-secondary" 
                             download>
                             Resumen CV
